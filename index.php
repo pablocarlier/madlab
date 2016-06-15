5b26bdfee4008cae64c57071622d360f4fc05652
pablocarlier <pablocarlier@gmail.com>
2016-06-15 16:17:19 +0200

    Minor Label Changes

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 		
 		<p>DC Spain Resource Site</p>
 		
-		<p>Welcome to the Front Page!</p>
+		<p>Welcome to the Main Page!</p>
 		
 		<a href="http://10.50.0.206/LabCalendar/Lab_Calendar.htm">Reserva tu slot en el Laboratorio</a>
 		
